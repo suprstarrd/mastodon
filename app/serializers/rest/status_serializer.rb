dc834deb5b30938c05cccbea66e4c4e54d5f0307
Essem <smswessem@gmail.com>
2025-03-05 17:34:24 -0600

    Fix reactions not showing when logged out

--- app/serializers/rest/status_serializer.rb
+++ app/serializers/rest/status_serializer.rb
@@ -177,7 +177,7 @@ class REST::StatusSerializer < ActiveModel::Serializer
   end
 
   def reactions
-    if relationships
+    if relationships && current_user?
       relationships.reactions_map[object.id] || []
     else
       object.reactions(current_user&.account&.id)
