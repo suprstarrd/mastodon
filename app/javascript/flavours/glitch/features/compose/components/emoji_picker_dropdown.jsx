3d56b7f63045d440752f41bf1e5fd5f55fbfb2bc
Essem <smswessem@gmail.com>
2026-03-04 14:14:30 -0600

    Consolidate title/icon props into single boolean

--- app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
+++ app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
@@ -8,6 +8,7 @@ import classNames from 'classnames';
 import { supportsPassiveEvents } from 'detect-passive-events';
 import Overlay from 'react-overlays/Overlay';
 
+import AddReactionIcon from '@/material-icons/400-24px/add_reaction.svg?react';
 import MoodIcon from '@/material-icons/400-20px/mood.svg?react';
 import { IconButton } from 'flavours/glitch/components/icon_button';
 import { injectIntl } from '@/flavours/glitch/components/intl';
@@ -28,6 +29,7 @@ const messages = defineMessages({
   objects: { id: 'emoji_button.objects', defaultMessage: 'Objects' },
   symbols: { id: 'emoji_button.symbols', defaultMessage: 'Symbols' },
   flags: { id: 'emoji_button.flags', defaultMessage: 'Flags' },
+  react: { id: 'status.react', defaultMessage: 'React' },
 });
 
 let EmojiPicker, Emoji; // load asynchronously
@@ -301,8 +303,7 @@ class EmojiPickerDropdown extends PureComponent {
     onPickEmoji: PropTypes.func.isRequired,
     onSkinTone: PropTypes.func.isRequired,
     skinTone: PropTypes.number.isRequired,
-    title: PropTypes.string,
-    icon: PropTypes.node,
+    react: PropTypes.bool,
     disabled: PropTypes.bool,
   };
 
@@ -365,8 +366,9 @@ class EmojiPickerDropdown extends PureComponent {
   };
 
   render() {
-    const { intl, onPickEmoji, onSkinTone, skinTone, frequentlyUsedEmojis, title: propsTitle, icon, disabled } = this.props;
-    const title = propsTitle ?? intl.formatMessage(messages.emoji);
+    const { intl, onPickEmoji, onSkinTone, skinTone, frequentlyUsedEmojis, react, disabled } = this.props;
+    const title = react ? intl.formatMessage(messages.react) : intl.formatMessage(messages.emoji);
+    const icon = react ? AddReactionIcon : MoodIcon;
     const { active, loading, placement } = this.state;
 
     return (
@@ -375,7 +377,7 @@ class EmojiPickerDropdown extends PureComponent {
           title={title}
           aria-expanded={active}
           active={active}
-          iconComponent={icon || MoodIcon}
+          iconComponent={icon}
           onClick={this.onToggle}
           disabled={disabled}
           id="emoji"
